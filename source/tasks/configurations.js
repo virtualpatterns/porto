dfb16fa3d4f3b0692408c60ce92765e8d6260794
virtualpatterns <code@virtualpatterns.com>
2017-12-13 00:30:55 -0500

    Adding message

--- source/tasks/configurations.js
+++ source/tasks/configurations.js
@@ -1,10 +1,12 @@
 import 'babel-polyfill'
 import Jake from 'jake'
+import { Log } from 'mablung'
 
 namespace('configurations', () => {
 
   desc('Copy default configuration')
   task('default', [], {}, () => {
+    Log.debug('- Copying default configuration ...')
     Jake.cpR('./source/www/configurations/default.json', './source/www/configuration.json', { 'silent': true })
   })
 
