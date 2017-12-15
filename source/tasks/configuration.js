c055b2ec2628b517c10b6e0f7ed6947ff45e0196
virtualpatterns <code@virtualpatterns.com>
2017-12-15 00:21:11 -0500

    Adding anonymouse configuration

--- source/tasks/configuration.js
+++ source/tasks/configuration.js
@@ -4,6 +4,18 @@ import { Log } from 'mablung'
 
 namespace('configuration', () => {
 
+  desc('Copy 0.0.0.0 configuration')
+  task('0.0.0.0', [], {}, () => {
+    Log.debug('- Copying 0.0.0.0 configuration ...')
+    Jake.cpR('./source/www/configurations/0.0.0.0.json', './source/www/configuration.json', { 'silent': true })
+  })
+
+  desc('Copy anonymouse configuration')
+  task('anonymouse', [], {}, () => {
+    Log.debug('- Copying anonymouse configuration ...')
+    Jake.cpR('./source/www/configurations/anonymouse.json', './source/www/configuration.json', { 'silent': true })
+  })
+
   desc('Copy default configuration')
   task('default', [], {}, () => {
     Log.debug('- Copying default configuration ...')
